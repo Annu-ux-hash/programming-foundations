450c10667f9b183e316ec9d4126dc8fe153c11e3
Annu-ux-hash <kumawatannu736@gmail.com>
2026-01-01 14:51:46 +0530

    continue and some revised concept

---
+++ python/c_flow&loops/code.py
@@ -0,0 +1,4 @@
+#tabel 
+i = 77
+for i in range (1,11):
+    print("77 X",i,"=", 77*i)
---
+++ python/c_flow&loops/continue.py
@@ -0,0 +1,23 @@
+# i = 1
+# for i in range (0,21):
+#     if i % 2 != 0: # # i is divide by 2 then remainder  is not equal to zero = even (telling to skip odd no.)
+#         continue
+#     print(i)
+# output : 0 2 4 6 8 10 12 14 16 18 20
+
+# i = 1 
+# for i in range (0,21):
+#     if i % 2 == 0: # i is divide by 2 then remainder is equal to zero = even (telling to skip even no.)
+#         continue
+#     print(i)
+#  #output = 1 3 5 7 9 11 13 15 17 19
+
+'''
+#it is used when a statement is syntactically required, but you don’t want to write logic yet.
+pass : do nothing if want to write logic later you can 
+'''
+i = 1 
+for i in range(2,10):
+    if i == 2:
+        pass
+    print(i)
