37238155b0fe0249286017f349518351b41f87f6
Annu-ux-hash <kumawatannu736@gmail.com>
2026-01-02 19:06:26 +0530

    bad day

---
+++ python/string/String Slicing and Indexing.py
@@ -0,0 +1,27 @@
+#name = "Python"
+#.      012345 goes from  0 to n-1
+# example "abcdefgh"= 8-1=7
+#.    -(654321) formula = given value ex -3 add total character we have 6
+# = (-n)+totl characters 
+# = ex -4+6 = 2 [t] 
+name = "anandk"
+#       012345
+#     -(654321) 
+# called  string indexing
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[-2])
+
+
+#string silencing = you can extract part of string what you want 
+N = "hey anand how are you" # each space aslo have location 
+#.   0123456789--------- 21
+#print(N[0:n-1])
+# print(N[4:8])
+# print(N[4:9])
+# print(N[0:9])
+# print(N[10:])
+print(N[0:16:5])
---
+++ python/string/Strings in Python.py
@@ -0,0 +1,11 @@
+#string is most fundamental data type in python . 
+#string: sequence of character enclosed either in "" or '' or """ """/''' '''
+#" " single line string 
+# ''' multiline string '''
+name = "Anand" #single line 
+print(name)
+introduction = """ my name is anand 
+ i am student of bca 1st year 
+ i am very  enthusiastic to learn and grow 
+""" 
+print(introduction)
