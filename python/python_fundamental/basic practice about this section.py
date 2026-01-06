d98679ef42eb65dec0f101c8a51288178f07aa1f
Annu-ux-hash <kumawatannu736@gmail.com>
2026-01-07 01:26:46 +0530

    Create basic practice about this section.py

---
+++ python/python_fundamental/basic practice about this section.py
@@ -0,0 +1,53 @@
+#Q1 
+print("Hello, world! welcome to my python.")
+
+#Q2
+
+print('Twinkel, twinkel, little star,\nHow I wonder what you are!')
+
+#Q3
+
+name = 'Anand'
+age = 21
+height = 1.8034
+is_student = True 
+print("i am",name, ",",age,"yr old", "," "height:-",height, "i study 1st yr collece:-",is_student)
+
+
+#Q4
+
+num = "45"
+a = int(num)
+print(a + 10)
+
+#Q5
+
+fav_food = input("what your fav food:-")
+print("Wow! I also like",fav_food)
+
+#Q6
+
+a = int(input("enter 1st value:- "))
+b = int(input("enter 2st value:- "))
+print("Sum:-",a+b,"\n","Substraction:-", a-b,"\n","Product:-", a*b,"\n","Quotient:-", a/b,)
+
+#Q7
+
+print("Harry said, \"Python is awesome !\"\nThis is on a new line.\nThis is tab->\t<-here")
+
+#Q8
+
+a = int(input("enter a no. :- "))
+print("squre:-",a*a,"\n","cube:-",a*a*a,)
+# square:- n**2, cube:- n**3
+
+#Q9
+
+# Mark True or False:
+# Python code must always end with a semicolon The  "F"
+# "#"symbol is used for comments in Python. "T"
+# "123" and 123 are the same in Python. "F"
+# The * operator is used for multiplication. "T"
+# \n creates a new line. "T"
+# Variables in Python can start with numbers. "F"
+# int("10") + 5 gives 15 ."T"
